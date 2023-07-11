7eea097aed9d8995d63592ea97aa1095a2e3665c
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-11 11:30:48 +0300

    Layid out dashboard skeleton

---
+++ src/components/dashboard/topnav/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="row justify-content-between">
+      <div className="col">
+        <div className="text-start h2">
+            
+            Dawa Drop
+        </div>
+      </div>
+      <div className="col">
+        <div className="text-end">
+            End
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/screens/dashboard/DashBoard.jsx
+++ src/screens/dashboard/DashBoard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import SideBar from "../../components/dashboard/sidebar/SideBar";
+import Content from "../../components/dashboard/content/Content";
+import NavBar from "../../components/dashboard/topnav/NavBar";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  return (
+    <div className="contaner">
+      <div className="row border">
+        <NavBar />
+      </div>
+      <div className="row">
+        <div className="col-lg-2 border ">
+          <SideBar />
+        </div>
+        <div className="col-lg-10 border">
+          <Content />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;

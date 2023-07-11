c8806107cc714687e9eb4cd11493c1624d57cb7a
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-11 10:16:17 +0300

    Installed bootstrap and fontawsome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigator from "./navigation/Navigator";
 function App() {
   return (
-    <div className="App" style={{ flex: 1 }}>
-      
+    <div className="App">
+      <Router>
+        <Navigator />
+      </Router>
     </div>
   );
 }
